8b101839a35f7662811e8090a815d7507cd1adff
togakyo <togakyo@gmail.com>
2020-06-16 01:32:42 +0900

    ・JSON出力に対応 ・Nsan向けモデル対応 　L11で　N向け有効化

--- tmp_sagyou/deep-sort-yolov4/sample_togakyo.py
+++ tmp_sagyou/deep-sort-yolov4/sample_togakyo.py
@@ -8,6 +8,7 @@ import warnings
 import cv2
 import numpy as np
 from PIL import Image
+#from Nyolo import YOLO
 from yolo import YOLO
 
 from deep_sort import preprocessing
@@ -23,6 +24,11 @@ import glob
 
 warnings.filterwarnings('ignore')
 
+@classmethod
+def getValue(cls, key, items):
+    values = [x['Value'] for x in items if 'Key' in x and 'Value' in x and x['Key'] == key]
+    return values[0] if values else None
+        
 def main(yolo, input):
 
     # Definition of the parameters
@@ -32,10 +38,13 @@ def main(yolo, input):
     
     # Deep SORT
     model_filename = './model_data/mars-small128.pb'
-    encoder = gdet.create_box_encoder(model_filename, batch_size=1)
+    cencoder = gdet.create_box_encoder(model_filename, batch_size=1)
+    pencoder = gdet.create_box_encoder(model_filename, batch_size=1)
     
-    metric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
-    tracker = Tracker(metric)
+    cmetric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
+    pmetric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
+    ctracker = Tracker(cmetric)
+    ptracker = Tracker(pmetric)
 
     tracking = True
     writeVideo_flag = True
@@ -50,107 +59,138 @@ def main(yolo, input):
     Pedestrian_result_ALL = []
     all_result = []
     
-    for i in range(len(cl_list)):
-        video_capture = cv2.VideoCapture(input)
+    video_capture = cv2.VideoCapture(input)
 
-        fps = 0.0
-        fps_imutils = imutils.video.FPS().start()
-        if writeVideo_flag:
-            fname = cl_list[i] + 'output_yolov4.mp4'
-            output_path = './output/'+ fname
-            video_FourCC = int(video_capture.get(cv2.CAP_PROP_FOURCC))
-            video_fps = video_capture.get(cv2.CAP_PROP_FPS)
-            video_size = (int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH)), int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    fps = 0.0
+    fps_imutils = imutils.video.FPS().start()
+    if writeVideo_flag:
+        fname = cl_list[i] + 'output_yolov4.mp4'
+        output_path = './output/'+ fname
+        video_FourCC = int(video_capture.get(cv2.CAP_PROP_FOURCC))
+        video_fps = video_capture.get(cv2.CAP_PROP_FPS)
+        video_size = (int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH)), int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+        
+        out = cv2.VideoWriter(output_path, video_FourCC, video_fps, video_size)
+        frame_index = -1
 
-            out = cv2.VideoWriter(output_path, video_FourCC, video_fps, video_size)
-            frame_index = -1
+    Nm_fr = 0
+    while True:
+        Nm_fr = Nm_fr + 1
+        
+        ret, frame = video_capture.read()  # frame shape 1920*1216*3
+        
+        if ret != True:
+            break
+        
+        t1 = time.time()
+        image = Image.fromarray(frame[...,::-1])
+        
+        cboxes, cconfidence, cclasses = yolo.detect_image(image, cl_list[1])
+        pboxes, pconfidence, pclasses = yolo.detect_image(image, cl_list[0])
 
-        while True:
-            ret, frame = video_capture.read()  # frame shape 1920*1216*3
-            if ret != True:
-                break
+        if tracking:
+            cfeatures = cencoder(frame, cboxes)
+            pfeatures = pencoder(frame, pboxes)
 
-            t1 = time.time()
-            image = Image.fromarray(frame[...,::-1])
-            
-            cl_predstr = cl_list[i]
-            boxes, confidence, classes = yolo.detect_image(image, cl_predstr)
+            cdetections = [Detection(cbbox, cconfidence, ceach_class, cfeature) for cbbox, cconfidence, ceach_class, cfeature in \
+                          zip(cboxes, cconfidence, cclasses, cfeatures)]
+            pdetections = [Detection(pbbox, pconfidence, peach_class, pfeature) for pbbox, pconfidence, peach_class, pfeature in \
+                          zip(pboxes, pconfidence, pclasses, pfeatures)]
+            #else:
+            #    detections = [Detection_YOLO(bbox, confidence, each_class) for bbox, confidence, each_class in \
+            #                 zip(boxes, confidence, classes)]
 
-            if tracking:
-                features = encoder(frame, boxes)
+            # Run non-maxima suppression.
+            cboxes = np.array([d.tlwh for d in cdetections])
+            cscores = np.array([d.confidence for d in cdetections])
+            cindices = preprocessing.non_max_suppression(cboxes, nms_max_overlap, cscores)
+            cdetections = [cdetections[i] for i in cindices]
 
-                detections = [Detection(bbox, confidence, each_class, feature) for bbox, confidence, each_class, feature in \
-                             zip(boxes, confidence, classes, features)]
-            else:
-                detections = [Detection_YOLO(bbox, confidence, each_class) for bbox, confidence, each_class in \
-                             zip(boxes, confidence, classes)]
-
-                # Run non-maxima suppression.
-                boxes = np.array([d.tlwh for d in detections])
-                scores = np.array([d.confidence for d in detections])
-                indices = preprocessing.non_max_suppression(boxes, nms_max_overlap, scores)
-                detections = [detections[i] for i in indices]
+            pboxes = np.array([d.tlwh for d in pdetections])
+            pscores = np.array([d.confidence for d in pdetections])
+            pindices = preprocessing.non_max_suppression(pboxes, nms_max_overlap, pscores)
+            pdetections = [pdetections[i] for i in pindices]
 
             if tracking:
                 # Call the tracker
-                tracker.predict()
-                tracker.update(detections)
+                ctracker.predict()
+                ctracker.update(cdetections)
+                
+                ptracker.predict()
+                ptracker.update(pdetections)
 
-                for track in tracker.tracks:
-                    if not track.is_confirmed() or track.time_since_update > 1:
+                for ctrack in ctracker.tracks:
+                    if not ctrack.is_confirmed() or ctrack.time_since_update > 1:
                         continue
-                    bbox = track.to_tlbr()
-                    cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (0, 0, 255), 10)
-                    cv2.putText(frame, "ID: " + str(track.track_id), (int(bbox[0]), int(bbox[1])), 0, \
+                    cbbox = ctrack.to_tlbr()
+                    cv2.rectangle(frame, (int(cbbox[0]), int(cbbox[1])), (int(cbbox[2]), int(cbbox[3])), (0, 0, 255), 3)
+                    cv2.putText(frame, "ID: " + str(ctrack.track_id), (int(cbbox[0]), int(cbbox[1])), 0, \
                                 1.5e-3 * frame.shape[0], (0, 0, 255), 3)
                     
-                    ID     = int(track.track_id)   
-                    top    = int(bbox[0]) 
-                    left   = int(bbox[1]) 
-                    bottom = int(bbox[2]) 
-                    right  = int(bbox[3]) 
+                    #OUTPUT TRACKING
+                    ID     = int(ctrack.track_id)   
+                    top    = int(cbbox[0]) 
+                    left   = int(cbbox[1]) 
+                    bottom = int(cbbox[2]) 
+                    right  = int(cbbox[3]) 
 
-                    if cl_predstr == 'Car':
-                        Car_result = {'id': ID, 'box2d': [left,top,right,bottom]}#予測結果
-                        Car_result_ALL.append(Car_result)
-                        
-                    elif cl_predstr == 'Pedestrian':
-                        Pedestrian_result = {'id': ID, 'box2d': [left,top,right,bottom]}#予測結果
-                        Pedestrian_result_ALL.append(Pedestrian_result)
+                    Car_result = {'id': ID, 'box2d': [left,top,right,bottom]}#予測結果
+                    print("Frame no. = ", Nm_fr)
+                    print("Car_result = ", Car_result)
+                    Car_result_ALL.append(Car_result)
+                    
+                for ptrack in ptracker.tracks:
+                    if not ptrack.is_confirmed() or ptrack.time_since_update > 1:
+                        continue
+                    pbbox = ptrack.to_tlbr()
+                    cv2.rectangle(frame, (int(pbbox[0]), int(pbbox[1])), (int(pbbox[2]), int(pbbox[3])), (0, 0, 255), 3)
+                    cv2.putText(frame, "ID: " + str(ptrack.track_id), (int(pbbox[0]), int(pbbox[1])), 0, \
+                                1.5e-3 * frame.shape[0], (0, 255, 0), 3)
+                    
+                    #OUTPUT TRACKING
+                    ID     = int(ptrack.track_id)   
+                    top    = int(pbbox[0]) 
+                    left   = int(pbbox[1]) 
+                    bottom = int(pbbox[2]) 
+                    right  = int(pbbox[3]) 
 
-            for det in detections:
-                bbox = det.to_tlbr()
-                score = "%.2f" % round(det.confidence * 100, 2) + "%"
-                cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (255, 0, 0), 10)
-                if len(classes) > 0:
-                    each_class = det.cls
-                    cv2.putText(frame, str(each_class) + " " + score, (int(bbox[0]), int(bbox[3])), 0, \
-                                1.5e-3 * frame.shape[0], (255, 0, 0), 3)
-
+                    Pedestrian_result = {'id': ID, 'box2d': [left,top,right,bottom]}#予測結果
+                    print("Frame no. = ", Nm_fr)
+                    print("Pedestrian_result = ", Pedestrian_result)
+                    Pedestrian_result_ALL.append(Pedestrian_result)
+                
+                                        
+            #for det in detections:
+                #bbox = det.to_tlbr()
+                #score = "%.2f" % round(det.confidence * 100, 2) + "%"
+                #cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (255, 0, 0), 10)
+                #if len(classes) > 0:
+                #    each_class = det.cls
+                #    cv2.putText(frame, str(each_class) + " " + score, (int(bbox[0]), int(bbox[3])), 0, \
+                #                1.5e-3 * frame.shape[0], (255, 0, 0), 3)
+            
+            # Each frame result
+            all_result.append({'Car': Car_result_ALL, 'Pedestrian': Pedestrian_result_ALL})
+            
             if writeVideo_flag:
                 # save a frame
                 out.write(frame)
                 frame_index = frame_index + 1
             
-            all_result = {'Car': Car_result_ALL, 'Pedestrian': Pedestrian_result_ALL}
-            predictions.append({input: all_result})
-
             fps_imutils.update()
 
             fps = (fps + (1./(time.time()-t1))) / 2
-            print(cl_predstr)
             print("     FPS = %f"%(fps))
-            
-        video_capture.release()
-        
-        fps_imutils.stop()
-        print('imutils FPS: {}'.format(fps_imutils.fps()))
 
         if writeVideo_flag:
             out.release()
-    
-    return  
 
+    video_capture.release()
+        
+    fps_imutils.stop()
+    print('imutils FPS: {}'.format(fps_imutils.fps()))
+    
+    return {input: all_result}
 
 if __name__ == '__main__':
 
@@ -161,12 +201,13 @@ if __name__ == '__main__':
     data_path = './data'
     
     #複数のファイルに対応済み
-    videos = glob.glob(data_path+'/*.mp4')
+    videos = sorted(glob.glob(data_path+'/*.mp4'))
     
     for i in range(len(videos)):
         video_path = videos[i]
 
         Output = main(YOLO(), video_path)
+        print("Output = ", Output)
     
         if i == 0:#最初はキーを指定して辞書作成
             Output_list = Output
@@ -174,5 +215,5 @@ if __name__ == '__main__':
             Output_list.update(Output)
     
     print("＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊")
-    with open('../output/prediction.json', 'w') as f:
+    with open('./output/prediction.json', 'w') as f:
         json.dump(Output_list, f)
