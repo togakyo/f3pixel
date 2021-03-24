fd3e871df67b01c9c86933d2bb886e3c22651e8d
togakyo <togakyo@gmail.com>
2021-03-24 21:04:44 +0900

    _10_aPerformance

--- javascript/statement.js
+++ javascript/statement.js
@@ -5,23 +5,23 @@ var plays = '{"hamlet":{"name": "Hamlet", "type": "tragedy"},"as-like":{"name": 
 var jsonObject_invoices = JSON.parse(invoices);　　　//HACK: invoices JSON読み込みを想定
 var jsonObject_plays = JSON.parse(plays);　　　      //HACK: plays JSON読み込みを想定
 
-function amountFor (perf, play){
+function amountFor (aPerformance, play){
     let result = 0;
 
 
     switch (play.type){
         case "tragedy":
             result = 40000;
-            if(perf.audience > 30){
-                result += 1000 * (perf.audience -30);
+            if(aPerformance.audience > 30){
+                result += 1000 * (aPerformance.audience -30);
             }
             break;
         case "comedy":
             result = 30000;
-            if(perf.audience > 20){
-                result += 10000 + 500 * (perf.audience -20);
+            if(aPerformance.audience > 20){
+                result += 10000 + 500 * (aPerformance.audience -20);
             }
-            result += 300 * perf.audience;
+            result += 300 * aPerformance.audience;
             break;
         default:
             throw new Error("unknown type:" + play.type + "\n");
@@ -40,7 +40,7 @@ function statement (invoices, plays){
                              minimumIntegerDigits: 2 }).format;
     for (let perf of invoices.performances) {
         const play = plays[perf.playID];
-        
+
 
         let thisAmount = amountFor (perf, play);
 
