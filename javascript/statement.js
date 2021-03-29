217e99819a428862084357a0c06a7939fae9e2c2
togakyo <togakyo@gmail.com>
2021-03-30 02:12:06 +0900

    ＿p20＿変数のインライン化

--- javascript/statement.js
+++ javascript/statement.js
@@ -61,9 +61,8 @@ function statement (invoices, plays){
         totalAmount += amountFor (perf);
     }
 
-    let volumeCredits = totalVolumeCredits();
     result += " Amount owed is " + usd(totalAmount) + "\n";
-    result += " You earned " + volumeCredits + " " +  "credits\n";
+    result += " You earned " + totalVolumeCredits() + " " +  "credits\n";
     return result ;
 }
 
