50e0af619c929d388364c002b08c07ce024c1686
togakyo <togakyo@gmail.com>
2021-03-30 03:29:52 +0900

    _p31_ふたつのファイル分離前

--- javascript/statement.js
+++ javascript/statement.js
@@ -5,6 +5,28 @@ var plays = '{"hamlet":{"name": "Hamlet", "type": "tragedy"},"as-like":{"name": 
 var jsonObject_invoices = JSON.parse(invoices);　　　//HACK: invoices JSON読み込みを想定
 var jsonObject_plays = JSON.parse(plays);　　　      //HACK: plays JSON読み込みを想定
 
+function htmlStatement(invoices, plays) {
+    return renderHtml(createStatementData(invoices, plays))
+
+    function renderHtml(data){
+        let = result = '<h1>Statement for data.cunstomer<h1>\n';
+        result += "<table>\n";
+        result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+
+        for (let perf of data.performances){
+            result += '   <tr><td>perf.play.name</td><td>perf.audience</td>';
+            result += '<td>usd(perf.amount)</td></tr>\n';
+        }
+        result += "</table>\n";
+        result += '<p>Amount owed is <em>usd(data.totalAmount)</em></p>\n';
+        result += '<p>You earned <em>data.totalVolumeCredits<em> credits</p>\n'
+
+        return result;
+    }
+}
+function usd(eNumber){
+    return new Intl.NumberFormat("en-US",{ style: "currency", currency: "USD", minimumIntegerDigits: 2 }).format(eNumber/100);
+}
 function statement (invoices, plays){
     return renderPlainText(createStatementData(invoices, plays));
 
@@ -86,10 +108,6 @@ function renderPlainText(data, plays){
     result += " Amount owed is " + usd(data.totalAmount) + "\n";
     result += " You earned " + data.totalVolumeCredits + " " +  "credits\n";
     return result ;
-
-    function usd(eNumber){
-        return new Intl.NumberFormat("en-US",{ style: "currency", currency: "USD", minimumIntegerDigits: 2 }).format(eNumber/100);
-    }
 }
 
 
