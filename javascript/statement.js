da5f7a7a3a63ed2d63e6795859bd0d67f8697e19
togakyo <togakyo@gmail.com>
2021-03-31 21:42:15 +0900

    ファイル分岐_html出力

--- javascript/statement.js
+++ javascript/statement.js
@@ -1,102 +1,17 @@
-
 var invoices = '{"customer": "BigCo","performances": [{"playID": "hamlet","audience": 55},{"playID": "as-like","audience": 35},{"playID": "othello","audience": 40}]}';
 var plays = '{"hamlet":{"name": "Hamlet", "type": "tragedy"},"as-like":{"name": "As You Like It", "type": "comedy"},"othello": {"name": "Othello", "type": "tragedy"}}';
 
 var jsonObject_invoices = JSON.parse(invoices);　　　//HACK: invoices JSON読み込みを想定
 var jsonObject_plays = JSON.parse(plays);　　　      //HACK: plays JSON読み込みを想定
 
-function htmlStatement(invoices, plays) {
-    return renderHtml(createStatementData(invoices, plays))
+//Served を立ち上げる
+import createStatementData from "./createStatementData.js";
 
-    function renderHtml(data){
-        let = result = '<h1>Statement for data.cunstomer<h1>\n';
-        result += "<table>\n";
-        result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
-
-        for (let perf of data.performances){
-            result += '   <tr><td>perf.play.name</td><td>perf.audience</td>';
-            result += '<td>usd(perf.amount)</td></tr>\n';
-        }
-        result += "</table>\n";
-        result += '<p>Amount owed is <em>usd(data.totalAmount)</em></p>\n';
-        result += '<p>You earned <em>data.totalVolumeCredits<em> credits</p>\n'
-
-        return result;
-    }
-}
 function usd(eNumber){
     return new Intl.NumberFormat("en-US",{ style: "currency", currency: "USD", minimumIntegerDigits: 2 }).format(eNumber/100);
 }
 function statement (invoices, plays){
     return renderPlainText(createStatementData(invoices, plays));
-
-    function createStatementData(invoices, plays){
-        const statementData = {} ;
-        statementData.customer = invoices.customer;
-        statementData.performances = invoices.performances.map(enrichPerformance);
-        statementData.totalAmount = totalAmount(statementData);
-        statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-        return statementData;    
-    }
-
-    function enrichPerformance(aPerformance){
-        const result = Object.assign({}, aPerformance);
-        result.play = playFor(result);
-        result.amount = amountFor(result);
-        result.volumeCredits = volumeCreditsFor(result);
-        return result;
-    }
-
-    function playFor(aPerformance){
-        return plays[aPerformance.playID];
-    }
-
-    function amountFor(aPerformance){
-        let result = 0;
-    
-        switch (aPerformance.play.type){
-            case "tragedy":
-                result = 40000;
-                if(aPerformance.audience > 30){
-                    result += 1000 * (aPerformance.audience -30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if(aPerformance.audience > 20){
-                    result += 10000 + 500 * (aPerformance.audience -20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error("unknown type:" + aPerformance.play.type + "\n");
-        }
-    
-        return result;
-    }
-
-    function volumeCreditsFor(ePerformance){
-        let result = 0;
-        result += Math.max(ePerformance.audience - 30, 0) ;
-        if ("comedy" === ePerformance.play.type) result += Math.floor(ePerformance.audience / 5) ; 
-        return result;
-    }
-
-    function totalAmount(data){
-        let result = 0;
-        for (let perf of data.performances) {
-            result += perf.amount;
-        }
-        return result;
-    }
-    function totalVolumeCredits(data){
-        let result = 0 ;
-        for (let perf of data.performances) {
-            result += perf.volumeCredits;
-        }
-        return result;
-    }
-
 }
 
 function renderPlainText(data, plays){
@@ -110,9 +25,31 @@ function renderPlainText(data, plays){
     return result ;
 }
 
+function renderHtml (data){
+    let result = `<h1>Statement for ${data.customer}</h1>\n`; 
+    result += "<table>\n";
+    result+= "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+    for (let perf of data.performances) {    
+        result += `<tr><td>${perf.play.name}</td><td>${usd(perf.amount)}</td><td>(${perf.audience} seats)</td></tr>\n`;
+    }
+    result += "</table>\n";
+    result += `<p>Amount owed is  <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>You earned <em>${data.totalVolumeCredits} "credits</em></p>\n`;
+    return result ;
+}
+
+function htmlStatement(invoices, plays) {
+    return renderHtml(createStatementData(invoices, plays))
+}
+
+//function usd(eNumber){
+//    return new Intl.NumberFormat("en-US",{ style: "currency", currency: "USD", minimumIntegerDigits: 2 }).format(eNumber/100);
+//}
 
 
 let test = statement(jsonObject_invoices, jsonObject_plays);
+let test_html = htmlStatement(jsonObject_invoices, jsonObject_plays);
+document.getElementById("box").innerHTML = test_html ;//
 console.log(test);
 
 //invoices = '/Users/togashikyousuke/Desktop/f3pixel/javascript/invoices.json';
