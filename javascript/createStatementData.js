82d87df2425e114681a8905892495f3bf3377fc3
togakyo <togakyo@gmail.com>
2021-04-06 09:53:26 +0900

    p_44_１章完

--- javascript/createStatementData.js
+++ javascript/createStatementData.js
@@ -1,51 +1,75 @@
 class PerformanceCalculator {
-    constructor(ePerformance) {
+    constructor(aPerformance, aPlay) {
         this.performance = aPerformance;
-    }
-}
-export default function createStatementData(invoices, plays){
-    const calculator = new PerformanceCalculator(ePerformance);
-    const result = {} ;
-    statementData.customer = invoices.customer;
-    statementData.performances = invoices.performances.map(enrichPerformance);
-    statementData.totalAmount = totalAmount(result);
-    statementData.totalVolumeCredits = totalVolumeCredits(result);
-    return result;
-    
-    function enrichPerformance(aPerformance){
-        const result = Object.assign({}, aPerformance);
-        result.play = playFor(result);
-        result.amount = amountFor(result);
-        result.volumeCredits = volumeCreditsFor(result);
-        return result;
+        this.play = aPlay;
     }
 
+    get amount(){
+        throw new Error("サブクラスの責務");
+    }
+
+    get volumeCredits() {
+        return Math.max(this.performance.audience - 30, 0) ;;
+    }
+}
+
+function createPerformanceCalculator(aPerformance, aPlay){
+    switch(aPlay.type) {
+        case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+        case "comedy": return new ComedyCalculator(aPerformance, aPlay);
+        default:
+            throw new Error("未知の演劇種類"+ aPlay.type); 
+
+    }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 40000;
+        if(this.performance.audience > 30){
+            result += 1000 * (this.performance.audience -30);
+        }
+        return result;
+    }
+}
+class ComedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 30000;
+
+        if(this.performance.audience > 20){
+            result += 10000 + 500 * (this.performance.audience -20);
+        }
+        result += 300 * this.performance.audience;
+        return result;
+    }
+    get volumeCredits() {
+        return super.volumeCredits + Math.floor(this.performance.audience / 5);
+    }
+}
+
+export default function createStatementData(invoices, plays){
+    const result = {} ;
+    result.customer = invoices.customer;
+    result.performances = invoices.performances.map(enrichPerformance);
+    result.totalAmount = totalAmount(result);
+    result.totalVolumeCredits = totalVolumeCredits(result);
+    return result;
+
+    function enrichPerformance(aPerformance){
+        const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance));
+        const result = Object.assign({}, aPerformance);
+        result.play = calculator.play;
+        result.amount = calculator.amount;
+        result.volumeCredits = calculator.volumeCredits;
+        return result;
+    }
+    
     function playFor(aPerformance){
         return plays[aPerformance.playID];
     }
 
-    function amountFor(aPerformance){
-        let result = 0;
-    
-        switch (aPerformance.play.type){
-            case "tragedy":
-                result = 40000;
-                if(aPerformance.audience > 30){
-                    result += 1000 * (aPerformance.audience -30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if(aPerformance.audience > 20){
-                    result += 10000 + 500 * (aPerformance.audience -20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error("unknown type:" + aPerformance.play.type + "\n");
-        }
-    
-        return result;
+    function amountFor(aPerformance) {
+        return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
     }
 
     function volumeCreditsFor(ePerformance){
@@ -56,9 +80,11 @@ export default function createStatementData(invoices, plays){
     }
 
     function totalAmount(data){
-        return data.performances.reduce((toral, p) => total + p.amount, 0);
+        return data.performances.reduce((total, p) => total + p.amount, 0);
     }
     function totalVolumeCredits(data){
         return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
     }
 }
+
+
