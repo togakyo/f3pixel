71b685806cda0f0ef50cee722e3aba140e4c8741
togakyo <togakyo@gmail.com>
2021-04-13 21:11:02 +0900

    errortest

--- java/sample_codeMaven/test/src/main/java/test/Dollar.java
+++ java/sample_codeMaven/test/src/main/java/test/Dollar.java
@@ -21,9 +21,9 @@ class Dollar {
     {
         this.amount = amount;
     }
-    void times(int multiplier)
+    Dollar times(int multiplier)
     {
         amount *= multiplier;
-
+        return null;
     }
 }
