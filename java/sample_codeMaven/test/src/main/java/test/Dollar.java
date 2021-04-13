ecc83dd2c71de882df95f2ff66f6a0f9be787e11
togakyo <togakyo@gmail.com>
2021-04-13 20:54:42 +0900

    p9_timesメソッドへ移動

--- java/sample_codeMaven/test/src/main/java/test/Dollar.java
+++ java/sample_codeMaven/test/src/main/java/test/Dollar.java
@@ -16,13 +16,14 @@
 package test;
  
 class Dollar {
-    int amount = 10;
+    int amount;
     Dollar(int amount)
     {
 
     }
     void times(int multiplier)
     {
+        amount = 5 * 2;
 
     }
 }
