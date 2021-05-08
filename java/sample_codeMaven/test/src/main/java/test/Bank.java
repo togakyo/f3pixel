5d95f3b8173ba0093ccce6f1ba10f76f5582a874
togakyo <togakyo@gmail.com>
2021-05-08 21:22:29 +0900

    １４章の途中　NGケース

--- java/sample_codeMaven/test/src/main/java/test/Bank.java
+++ java/sample_codeMaven/test/src/main/java/test/Bank.java
@@ -4,6 +4,9 @@ class Bank
 {
     Money reduce(Expression source, String to)
     {
-        return Money.dollar(10);
+        //if (source instanceof Money)
+        //    return (Money) source;
+        Sum sum = (Sum) source;
+        return sum.reduce(to);
     }
 }
---
+++ java/sample_codeMaven/test/src/main/java/test/Sum.java
@@ -0,0 +1,17 @@
+package test;
+
+class Sum implements Expression
+{
+    Money augend;
+    Money addend;
+    Sum(Money augend, Money addend)
+    {
+        this.augend = augend;
+        this.addend = addend;
+    }
+    Money reduce(String to)
+    {
+        int amount = augend.amount + addend.amount;
+        return new Money(amount, to);
+    }
+}
