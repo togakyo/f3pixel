e9658daa96770bbc41cb091ff705f5e3d2d2b994
togakyo <togakyo@gmail.com>
2021-05-08 21:23:04 +0900

    １４章　途中OKケース

--- java/sample_codeMaven/test/src/main/java/test/Bank.java
+++ java/sample_codeMaven/test/src/main/java/test/Bank.java
@@ -4,8 +4,8 @@ class Bank
 {
     Money reduce(Expression source, String to)
     {
-        //if (source instanceof Money)
-        //    return (Money) source;
+        if (source instanceof Money)
+            return (Money) source;
         Sum sum = (Sum) source;
         return sum.reduce(to);
     }
