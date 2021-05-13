3b3d28b6e015d206b9b644ed099631a7a60dbac7
togakyo <togakyo@gmail.com>
2021-05-14 00:09:17 +0900

    18章終了

--- Python/xunit.py
+++ Python/xunit.py
@@ -1,16 +1,35 @@
-class Wasrun:
+class TestCase:
+    def __init__(self, name):
+        self.name = name
+    def run(self):
+        method = getattr(self, self.name)
+        method()
+
+class WasRun(TestCase):
     #pass
     def __init__(self, name):
         #pass
         self.wasRun = None
-    def run(self):
-        self.testMethod()
+        #self.name = name
+        super().__init__(name)
+    #def run(self):
+        #self.testMethod()
+    #    method = getattr(self, self.name)
+    #    method()
     def testMethod(self):
         #pass
         self.wasRun = 1
-    
 
-test = Wasrun("testMethod")
-print(test.wasRun)
-test.run()
-print(test.wasRun)
+class TestCaseTest(TestCase):
+    def testRunning(self):
+        test = WasRun("testMethod")
+        assert(not test.wasRun)
+        test.run()
+        assert(test.wasRun)
+
+TestCaseTest("testRunning").run()
+
+#test = Wasrun("testMethod")
+#print(test.wasRun)
+#test.run()
+#print(test.wasRun)
