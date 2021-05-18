2dbc73a6fc046a70388e6ef8b677539157bbd5dc
togakyo <togakyo@gmail.com>
2021-05-18 23:52:04 +0900

    20章終了

--- Python/xunit.py
+++ Python/xunit.py
@@ -3,21 +3,26 @@ class TestCase:
         self.name = name
     def setUp(self):
         pass
+    def tearDown(self):
+        pass
     def run(self):
         self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
 
 class WasRun(TestCase):
     def setUp(self):
         self.log = "setUp "
     def testMethod(self):
         self.log = self.log + "testMethod "
+    def tearDown(self):
+        self.log = self.log + "tearDown "
 
 class TestCaseTest(TestCase):
     def testTemplateMethod(self):
         test = WasRun("testMethod")
         test.run()
-        assert("setUp testMethod " == test.log)
+        assert("setUp testMethod tearDown " == test.log)
 
 TestCaseTest("testTemplateMethod").run()
