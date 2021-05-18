1d3d3336723fc709eb4f40c4371f0540bb187048
togakyo <togakyo@gmail.com>
2021-05-18 23:59:53 +0900

    21章_164

--- Python/xunit.py
+++ Python/xunit.py
@@ -10,6 +10,7 @@ class TestCase:
         method = getattr(self, self.name)
         method()
         self.tearDown()
+        return TestResult()
 
 class WasRun(TestCase):
     def setUp(self):
@@ -24,5 +25,15 @@ class TestCaseTest(TestCase):
         test = WasRun("testMethod")
         test.run()
         assert("setUp testMethod tearDown " == test.log)
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert("1 run, 0 failed" == result.summary())
+
+class TestResult:
+    def summary(self):
+        return "1 run, 0 failed"
+
 
 TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
