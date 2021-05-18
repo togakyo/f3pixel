5ef57e963522c29067bb38f4050cc1c91349c50f
togakyo <togakyo@gmail.com>
2021-05-18 23:47:51 +0900

    20章_途中_160

--- Python/xunit.py
+++ Python/xunit.py
@@ -10,21 +10,14 @@ class TestCase:
 
 class WasRun(TestCase):
     def setUp(self):
-        self.wasRun = None
         self.log = "setUp "
     def testMethod(self):
-        self.wasRun = 1
         self.log = self.log + "testMethod "
 
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun("testMethod")
-    def testRunning(self):
-        self.test.run()
-        assert(self.test.wasRun)
-    def testSetUp(self):
-        self.test.run()
-        assert("setUp testMethod " == self.test.log)
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert("setUp testMethod " == test.log)
 
-TestCaseTest("testRunning").run()
-TestCaseTest("testSetUp").run()
+TestCaseTest("testTemplateMethod").run()
