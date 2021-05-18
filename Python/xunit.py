9e1e587d49015b8429216026805e08ce6d1b6321
togakyo <togakyo@gmail.com>
2021-05-19 08:13:05 +0900

    ・testFailedResultFormattingのスペルミス修正 ・２３章終了

--- Python/xunit.py
+++ Python/xunit.py
@@ -1,3 +1,13 @@
+class TestResult:
+    def __init__(self):
+        self.runCount = 0
+        self.errorCount = 0
+    def testStarted(self):
+        self.runCount = self.runCount + 1
+    def testFailed(self):
+        self.errorCount = self.errorCount + 1
+    def summary(self):
+        return "%d run, %d failed" % (self.runCount, self.errorCount)
 
 class TestCase:
     def __init__(self, name):
@@ -16,53 +26,6 @@ class TestCase:
             result.testFailed()
         self.tearDown()
 
-class WasRun(TestCase):
-    def setUp(self):
-        self.log = "setUp "
-    def testMethod(self):
-        self.log = self.log + "testMethod "
-    def tearDown(self):
-        self.log = self.log + "tearDown "
-
-class TestCaseTest(TestCase):
-    def testTemplateMethod(self):
-        test = WasRun("testMethod")
-        test.run()
-        assert("setUp testMethod tearDown " == test.log)
-    def testResult(self):
-        test = WasRun("testMethod")
-        result = test.run()
-        assert("1 run, 0 failed" == result.summary())
-    def testFailedResult(self):
-        test = WasRun("testBrokenMethod")
-        result = test.run()
-        assert("1 run, 1 failed" == result.summary())
-    def testFailedResultFortmatting(self):
-        result = TestResult()
-        result.testStarted()
-        result.testFailed()
-        assert("1 run, 1 failed" == result.summary())
-    def testSuite(self):
-        suite = TestSuite()
-        suite.add(WasRun("testMethod"))
-        suite.add(WasRun("testBrokenMethod"))
-        result = TestResult()
-        suite.run(result)
-        assert("2 run, 1 failed" == result.summary())
-
-class TestResult:
-    def __init__(self):
-        self.runCount = 0
-        self.errorCount = 0
-    def testStarted(self):
-        self.runCount = self.runCount + 1
-    def testFailed(self):
-        self.errorCount = self.errorCount + 1
-    def testBrokenMethod(self):
-        raise Exception
-    def summary(self):
-        return "%d run, %d failed" % (self.runCount, self.errorCount)
-
 class TestSuite:
     def __init__(self):
         self.tests = []
@@ -71,6 +34,42 @@ class TestSuite:
     def run(self, result):
         for test in self.tests:
             test.run(result)
+
+class WasRun(TestCase):
+    def setUp(self):
+        self.log = "setUp "
+    def testMethod(self):
+        self.log = self.log + "testMethod "
+    def testBrokenMethod(self):
+        raise Exception
+    def tearDown(self):
+        self.log = self.log + "tearDown "
+
+class TestCaseTest(TestCase):
+    def setUp(self):
+        self.result = TestResult()
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run(self.result)
+        assert("setUp testMethod tearDown " == test.log)
+    def testResult(self):
+        test = WasRun("testMethod")
+        test.run(self.result)
+        assert("1 run, 0 failed" == self.result.summary())
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        test.run(self.result)
+        assert("1 run, 1 failed" == self.result.summary())
+    def testFailedResultFormatting(self):
+        self.result.testStarted()
+        self.result.testFailed()
+        assert("1 run, 1 failed" == self.result.summary())
+    def testSuite(self):
+        suite = TestSuite()
+        suite.add(WasRun("testMethod"))
+        suite.add(WasRun("testBrokenMethod"))
+        suite.run(self.result)
+        assert("2 run, 1 failed" == self.result.summary())
 
 suite = TestSuite()
 suite.add(TestCaseTest("testTemplateMethod"))
